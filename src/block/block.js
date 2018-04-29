10321273f75dd33847c66635a2640c3b5e32f308
Robert DeVore <deviodigital@gmail.com>
2018-04-29 14:34:01 -0400

    :ok_hand: IMPROVE: Updated example block name

--- src/block/block.js
+++ src/block/block.js
@@ -1,5 +1,5 @@
 /**
- * BLOCK: pillar-blocks
+ * BLOCK: pillar-press
  *
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
@@ -25,14 +25,14 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType( 'pillar-blocks/block', {
+registerBlockType( 'pillar-press/block', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
 	title: __( 'Example Block' ), // Block title.
 	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
 		__( 'pillar' ),
-		__( 'pillar-blocks' ),
+		__( 'pillar-press' ),
 		__( 'example' ),
 	],
 
@@ -45,7 +45,7 @@ registerBlockType( 'pillar-blocks/block', {
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
 	edit: function( props ) {
-		// Creates a <p class='wp-block-cgb-block-pillar-blocks'></p>.
+		// Creates a <p class='wp-block-cgb-block-pillar-press'></p>.
 		return (
 			<div className={ props.className }>
 				<p>— Hello from the backend.</p>
