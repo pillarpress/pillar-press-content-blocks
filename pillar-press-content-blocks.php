aeff99111ce2ee4e7af6c274f7978692cd75a252
Robert DeVore <deviodigital@gmail.com>
2018-04-29 22:16:35 -0400

    :book: DOC: Updated plugin file details

--- pillar-press-content-blocks.php
+++ pillar-press-content-blocks.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Pillar Press - Content Blocks
  * Plugin URI: https://pillar.press/
- * Description: Custom Gutenberg Blocks created by Robert DeVore.
- * Author: deviodigital
- * Author URI: http://www.robertdevore.com
+ * Description: The cornerstone of content creation for WordPress. Created by <a href="https://www.twitter.com/deviorobert" target="_blank">Robert DeVore</a>.
+ * Author: deviodigital, pillarpress
+ * Author URI: http://pillar.press
  * Version: 0.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package PPB
+ * @package PPCB
  */
 
 // Exit if accessed directly.
