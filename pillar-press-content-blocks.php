a703e74e747b4fbfb8a7b3e363c61f7c26f14590
Robert DeVore <deviodigital@gmail.com>
2018-05-01 12:08:41 -0400

    :book: DOC: Updated Author name and link

--- pillar-press-content-blocks.php
+++ pillar-press-content-blocks.php
@@ -3,8 +3,8 @@
  * Plugin Name: Pillar Press - Content Blocks
  * Plugin URI: https://pillar.press/
  * Description: The cornerstone of content creation for WordPress. Created by <a href="https://www.twitter.com/deviorobert" target="_blank">Robert DeVore</a>.
- * Author: Pillar Press
- * Author URI: http://pillar.press
+ * Author: Robert DeVore
+ * Author URI: http://www.robertdevore.com/
  * Version: 0.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
